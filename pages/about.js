75b73463edfe25f7c855c81805b94214a95399c1
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-04 10:50:09 -0500

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is an app to find the latest local events</p>
+            <p>verison 1</p>
+        </div>
+    )
+}
