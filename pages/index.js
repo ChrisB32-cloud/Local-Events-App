8ad7224f525db809aece25f457d49407b4f78d2b
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-05 12:09:17 -0500

    Displaying event data data.json

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
 import { API_URL } from '../config/index'
+import EventItem from '../components/EventItem'
 
 
 export default function HomePage({ events }) {
@@ -7,6 +9,17 @@ export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt}>
+          {evt.name}
+        </EventItem>
+      ))}
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All Events</a>
+        </Link>
+      )}
     </Layout>
   )
 }
@@ -15,7 +28,7 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`)
   const events = await res.json()
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1
   }
 }
