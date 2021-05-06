dce946d206107aff76d427bc1e1d07e48eac304b
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-06 12:10:50 -0500

    Instead of making and event component just decided to display items in [slug].js

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,53 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Layout from '../../components/Layout'
 import { API_URL } from '../../config/index'
+import styles from '../../styles/Event.module.css'
 
 export default function EventPage({ evt }) {
+
+    function deleteEvent() {
+        //
+    }
     console.log(evt);
     return (
         <Layout>
-            <h1>{evt.name}</h1>
+            <div className={styles.event}>
+                <div className={styles.controls}>
+                    <Link href={`/events/edit/${evt.id}`}>
+                        <a>
+                            <FaPencilAlt /> Edit Event
+                        </a>
+                    </Link>
+                    <a href="#" className={styles.delete} onClick={deleteEvent}>
+                        <FaTimes /> Delete Event
+                    </a>
+                </div>
+                <span> {evt.data} at {evt.time} </span>
+                <h1>{evt.name}</h1>
+                {evt.image && (
+                    <div className={styles.image}>
+                        <Image
+                            src={evt.image}
+                            width={960}
+                            height={600}
+                        />
+                    </div>
+                )}
+
+                <h3>Guest:</h3>
+                <p>{evt.performers}</p>
+                <h3>Description:</h3>
+                <p>{evt.description}</p>
+                <h3>Venue: {evt.venue}</h3>
+                <p>{evt.address}</p>
+
+                <Link href='/events'>
+                    <a className={styles.backs}>{'<'} Go Back</a>
+                </Link>
+            </div>
         </Layout>
     )
 }
@@ -40,7 +81,7 @@ export async function getStaticProps({ params }) {
 }
 
 
-// export async function getServerSideProps({ query: { slug } }) {
+// export async function getServerSideProps({query: {slug} }) {
 
 //     const res = await fetch(`${API_URL}/api/events/${slug}`)
 //     const events = await res.json()
