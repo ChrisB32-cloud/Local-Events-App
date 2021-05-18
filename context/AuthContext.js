858d2c8a4ab9c0700454727e13f4b0d9dda18173
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-18 11:45:20 -0500

    User is now able to logout, vai destroying cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -43,7 +43,14 @@ export const AuthProvider = ({ children }) => {
 
     // Sign Out a User
     const logout = async () => {
-        console.log('logout');
+        const res = await fetch(`${NEXT_URL}/api/logout`, {
+            method: 'POST'
+        })
+
+        if (res.ok) {
+            setUser(null)
+            router.push('/')
+        }
     }
 
     // Check if user is logged in
