e01de3bc0b4c714c5178da6cb320216b09053b84
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-05-20 12:12:26 -0500

    Created a DashboardEvent component to pass event details of a particular user for managing updates and deleting

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { parseCookies } from '../../helpers/index'
 import { API_URL } from '../../config/index'
+import DashboardEvent from '../../components/DashboardEvent'
 import Layout from '../../components/Layout'
+import styles from '../../styles/Dashboard.module.css'
+
 
 export default function DashboardPage({ events }) {
 
-    console.log(events);
+    // console.log(events);
+
+    const handleDelete = (id) => {
+        console.log(id);
+    }
+
     return (
         <Layout title='User Dashboard'>
-            <h1>Dashboard</h1>
+            <div className={styles.dash}>
+                <h1>Dashboard</h1>
+                <h3>My events</h3>
+                {events.map(evt => (
+                    <DashboardEvent key={evt.id} evt={evt} handleDelete={handleDelete} />
+                ))}
+            </div>
         </Layout>
     )
 }
